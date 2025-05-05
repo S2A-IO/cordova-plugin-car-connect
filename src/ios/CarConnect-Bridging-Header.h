7858c73e816b17e841a4f3bcd173355a8e426954
Khurram Ali <khurram.ali@rikfos.com>
2025-05-05 18:09:27 +0500

    Add cordova bridging header

---
+++ src/ios/CarConnect-Bridging-Header.h
@@ -0,0 +1,6 @@
+/**
+ * CarConnect-Bridging-Header.h ­– Bridge for cordova.
+ * 
+ * Copyright © 2025 RIKSOF. MIT License.
+ */
+#import <Cordova/CDV.h>
--- src/ios/CarConnect.swift
+++ src/ios/CarConnect.swift
@@ -33,7 +33,6 @@
  */
 
 import Foundation
-import Cordova
 import UIKit
 import CarPlay
 
